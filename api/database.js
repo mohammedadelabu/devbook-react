0d9ea4f491103409f8339146225021cd3737d46b
Emperor Hassy <decagon@Emperor-Hassys-MacBook-Air.local>
2022-01-30 07:33:43 +0100

    devEmperor Social App

---
+++ api/database.js
@@ -0,0 +1,13 @@
+// const mongoose = require("mongoose");
+
+// const db_connection = async () => {
+//   try {
+//     await mongoose.connect(process.env.MONGO_URL).then(() => {
+//       console.log("Connected to DB successfully");
+//     });
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
+
+// module.exports = db_connection;
